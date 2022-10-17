197cc44eff11b95352b5c4c225a0e2a84b1eba4e
navanjani <navanjani@outlook.com>
2022-10-17 22:47:41 +0200

    Fixing character/word counts

--- pages/character-count.tsx
+++ pages/character-count.tsx
@@ -3,11 +3,11 @@ import Box from "@mui/joy/Box";
 import IconButton from "@mui/joy/IconButton";
 import Textarea from "@mui/joy/Textarea";
 import Typography from "@mui/joy/Typography";
-import { Container, styled } from "@mui/material";
+import { Container, Stack, styled } from "@mui/material";
 import { NextPage } from "next";
 import Grid from "@mui/material/Grid";
 import { auto } from "@popperjs/core";
-import { borderRadius } from "@mui/system";
+import { borderRadius, width } from "@mui/system";
 import Paper from "@mui/material/Paper";
 import Link from "next/link";
 import Button from "@mui/material/Button";
@@ -36,63 +36,75 @@ const CharacterCount: NextPage = () => {
           fontWeight="lg"
           level="display1"
         >
-          Character Count
+          Character/Word Count
         </Typography>
       </Grid>
-      <Box
-        sx={{
-          border: "1px solid #00008B",
-          borderRadius: 10,
-          marginTop: 10,
-          backgroundColor: "#C0C0C0",
-          "&:hover": {
-            backgroundColor: "#7FFD4",
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      >
-        <Textarea
-          placeholder="Type in here…"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-          minRows={2}
-          maxRows={10}
+      <Grid>
+        <Box
           sx={{
-            width: 1150,
-            height: 500,
+            border: "1px solid #00008B",
+            borderRadius: 10,
+            marginTop: 10,
+            backgroundColor: "#C0C0C0",
+            "&:hover": {
+              backgroundColor: "#7FFD4",
+              opacity: [0.9, 0.8, 0.7],
+            },
           }}
-          endDecorator={
-            <Typography
-              textColor="#00008B"
-              fontSize="1.5rem"
-              fontWeight="lg"
-              sx={{ ml: "auto" }}
-            >
-              {text.length} character(s)
-              <br />
-              {numberOfWords} word(s)
-            </Typography>
-          }
-        />
-      </Box>
-
-      <Box
-        m={2}
-        p={2}
+        >
+          <Textarea
+            placeholder="Type in here…"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            minRows={2}
+            maxRows={10}
+            sx={{
+              width: 1150,
+              height: 500,
+            }}
+          />
+        </Box>
+      </Grid>
+      <Grid mt={5} container spacing={3}>
+        <Grid item xs={4}>
+          <Item
+            sx={{ border: "1px solid #00008B", fontSize: 16, color: "#00008B" }}
+          >
+            {text.length} character(s)
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item
+            sx={{ border: "1px solid #00008B", fontSize: 16, color: "#00008B" }}
+          >
+            {numberOfWords} word(s)
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item
+            sx={{ border: "1px solid #00008B", fontSize: 16, color: "#00008B" }}
+          >
+            bytes
+          </Item>
+        </Grid>
+      </Grid>
+      <Grid
+        width="300px"
+        bgcolor="#00FFFF"
+        alignItems="flex-end"
+        mt={5}
         sx={{
           border: "1px solid #00008B",
           borderRadius: 10,
-          marginTop: 10,
-          marginLeft: 120,
+          textAlign: "center",
+          fontSize: 16,
+          color: "#00008B",
         }}
       >
-        <Grid>
-          <Button>
-            {" "}
-            <Link href="/">Home </Link>
-          </Button>
-        </Grid>
-      </Box>
+        <Box>
+          <Link href="/">Home </Link>
+        </Box>
+      </Grid>
     </Container>
   );
 };
